166d56f1d2f420315d70a0af800465ad07db435a
kaanoner <oner.kaan.k@gmail.com>
2019-03-03 17:41:56 +0300

    date selection logic

--- weather-forecast/src/components/CityDetail.js
+++ weather-forecast/src/components/CityDetail.js
@@ -8,12 +8,15 @@ import { format, subHours } from 'date-fns';
 class CityDetail extends Component {
   constructor() {
     super();
+    this.state = {dateIndex: 0}
     this.renderForecast = this.renderForecast.bind(this)
   }
   componentDidMount() {
     this.props.requestCityDetail(this.props.match.params.cityId);
   }
-
+  handleClick(index) {
+    this.setState({dateIndex: index})
+  }
   render() {
     return (
       <main
@@ -22,10 +25,20 @@ class CityDetail extends Component {
       >
         <h1 className="text-4xl text-indigo-darker">5-day forecast for Buca</h1>
 
-        <div className="flex flex-col mt-4 -my-4">
-          <div className="w-1/5">
-            <div className="rounded shadow-md bg-indigo-lightest my-4 p-4 h-full flex flex-col">
-              <h2 className="text-lg font-bold text-indigo-darker">Saturday</h2>
+        <div className="flex flex-row mt-4">
+          {this.renderDates()}
+        </div>
+
+        {this.props.loading ? null : this.renderTable()}
+      </main>
+    );
+  }
+  renderDates() {
+    return this.props.cityWeatherDetail.slice(0,5).map((item,index)=> {
+        return (
+          <button key={item[0].dt_txt} className="w-1/5 p-2" onClick={()=>{this.handleClick(index)}}>
+            <div className="rounded shadow-md bg-indigo-lightest p-4 h-full flex flex-col">
+              <h2 className="text-lg font-bold text-indigo-darker">{format(new Date(item[0].dt * 1000), 'dddd')}</h2>
 
               <div className="mt-4 flex-1 inline-flex flex-row items-center">
                 <svg
@@ -36,7 +49,7 @@ class CityDetail extends Component {
                   <path d="M22.58 11.269c-6.237 0-11.311 5.075-11.311 11.312 0 6.237 5.074 11.312 11.311 11.312 6.236 0 11.311-5.074 11.311-11.312 0-6.238-5.075-11.312-11.311-11.312zM22.58 7.944a2.207 2.207 0 0 1-2.207-2.206V2.207a2.207 2.207 0 1 1 4.414 0v3.531a2.207 2.207 0 0 1-2.207 2.206zM22.58 37.215a2.207 2.207 0 0 0-2.207 2.207v3.53a2.207 2.207 0 1 0 4.414 0v-3.53a2.208 2.208 0 0 0-2.207-2.207zM32.928 12.231a2.208 2.208 0 0 1 0-3.121l2.497-2.497a2.207 2.207 0 1 1 3.121 3.121l-2.497 2.497a2.207 2.207 0 0 1-3.121 0zM12.231 32.93a2.205 2.205 0 0 0-3.121 0l-2.497 2.496a2.207 2.207 0 0 0 3.121 3.121l2.497-2.498a2.204 2.204 0 0 0 0-3.119zM37.215 22.58c0-1.219.988-2.207 2.207-2.207h3.531a2.207 2.207 0 1 1 0 4.413h-3.531a2.206 2.206 0 0 1-2.207-2.206zM7.944 22.58a2.207 2.207 0 0 0-2.207-2.207h-3.53a2.207 2.207 0 1 0 0 4.413h3.531a2.206 2.206 0 0 0 2.206-2.206zM32.928 32.93a2.208 2.208 0 0 1 3.121 0l2.497 2.497a2.205 2.205 0 1 1-3.121 3.12l-2.497-2.497a2.205 2.205 0 0 1 0-3.12zM12.231 12.231a2.207 2.207 0 0 0 0-3.121L9.734 6.614a2.207 2.207 0 0 0-3.121 3.12l2.497 2.497a2.205 2.205 0 0 0 3.121 0z" />
                 </svg>
 
-                <p className="text-4xl ml-3">17°</p>
+                <p className="text-4xl ml-3">{item[0].main.temp.toFixed(0)}°</p>
               </div>
 
               <div className="mt-4 flex flex-row text-grey-dark text-sm">
@@ -49,7 +62,7 @@ class CityDetail extends Component {
                     <path d="M6.979 195.181c30.288 25.979 64.616 55.426 131.017 55.426 66.403-.001 100.733-29.447 131.022-55.426 28.644-24.568 53.382-45.787 104.98-45.787 51.599 0 76.337 21.219 104.98 45.787 8.385 7.191 21.011 6.225 28.202-2.159 7.191-8.385 6.225-21.011-2.16-28.202-30.289-25.979-64.619-55.426-131.022-55.426-66.403 0-100.733 29.446-131.022 55.426-28.644 24.568-53.382 45.787-104.98 45.787-51.597 0-76.332-21.218-104.974-45.787-8.384-7.191-21.01-6.226-28.202 2.159-7.192 8.384-6.225 21.011 2.159 28.202z" />
                     <path d="M6.979 85.787c30.287 25.98 64.615 55.426 131.017 55.426 66.403 0 100.733-29.446 131.022-55.426C297.662 61.219 322.399 40 373.998 40c51.597 0 76.335 21.218 104.98 45.787 8.385 7.193 21.01 6.224 28.201-2.161 7.192-8.384 6.223-21.012-2.161-28.202C474.729 29.444 440.399 0 373.998 0c-66.403 0-100.733 29.446-131.022 55.426-28.644 24.568-53.382 45.787-104.98 45.787-51.596 0-76.332-21.218-104.974-45.787-8.384-7.192-21.011-6.226-28.202 2.159-7.192 8.384-6.225 21.011 2.159 28.202zM505.02 274.213c-30.289-25.979-64.619-55.426-131.022-55.426-66.403 0-100.733 29.446-131.022 55.426C214.332 298.781 189.594 320 137.996 320c-51.594 0-76.331-21.218-104.973-45.785l-.002-.002c-8.384-7.189-21.009-6.223-28.201 2.16-7.191 8.384-6.224 21.011 2.16 28.202C37.268 330.555 71.597 360 137.996 360c66.403 0 100.733-29.446 131.022-55.426 28.644-24.568 53.382-45.787 104.98-45.787 51.599 0 76.337 21.219 104.98 45.787 8.385 7.191 21.011 6.225 28.202-2.159 7.191-8.385 6.225-21.011-2.16-28.202z" />
                   </svg>
-                  10 km/h
+                  {item[0].wind.speed.toFixed(0)} km/h
                 </div>
 
                 <div className="ml-4 flex flex-row items-center">
@@ -63,18 +76,14 @@ class CityDetail extends Component {
                       fillRule="nonzero"
                     />
                   </svg>
-                  45%
+                  {item[0].main.humidity.toFixed(0)}%
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        {this.props.loading ? null : this.renderTable()}
-      </main>
-    );
+          </button>
+        )
+    })
   }
-
   renderTable() {
     return (
       <div className="mt-4">
@@ -99,8 +108,8 @@ class CityDetail extends Component {
   }
   
   renderForecast() {
-    return this.props.cityWeatherDetail[0].map((item,index) => {
-      const dateForDayName = new Date(this.props.cityWeatherDetail[0][0].dt * 1000);
+    return this.props.cityWeatherDetail[this.state.dateIndex].map((item,index) => {
+      const dateForDayName = new Date(this.props.cityWeatherDetail[this.state.dateIndex][0].dt * 1000);
       const date = new Date(item.dt * 1000);
       
       return (
@@ -111,12 +120,27 @@ class CityDetail extends Component {
             {format(subHours(date, 3), 'HH:mm')}-{format(date, 'HH:mm')}
           </td>
           <td>{this.renderWeatherIcon(item.weather[0].icon)}</td>
-          <td>{item.main.temp}°</td>
-          <td>{item.main.humidity}</td>
+          <td>{item.main.temp.toFixed(0)}°</td>
+          <td>{item.main.humidity.toFixed(0)}</td>
           <td>
+            <svg style={{ transform: `rotate(${item.wind.deg}deg)` }} className="w-12" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fillRule="evenodd">
+                <path d="M256 0C115.3 0 0 115.3 0 256s115.3 256 256 256 256-115.3 256-256S396.7 0 256 0z" fill="#68544F"/>
+                <path d="M512 256c0 140.7-115.3 256-256 256V0c140.7 0 256 115.3 256 256z" fill="#53433F"/>
+                <path d="M256 61C148.598 61 61 148.598 61 256c0 107.398 87.598 195 195 195s195-87.602 195-195c0-107.402-87.598-195-195-195z" fill="#EDE9E8"/>
+                <path d="M451 256c0 107.398-87.598 195-195 195V61c107.402 0 195 87.598 195 195z" fill="#DFD7D5"/>
+                <path d="M271 436v61c0 8.398-6.598 15-15 15-8.402 0-15-6.602-15-15v-61c0-8.402 6.598-15 15-15 8.402 0 15 6.598 15 15zM271 15v61c0 8.398-6.598 15-15 15-8.402 0-15-6.602-15-15V15c0-8.402 6.598-15 15-15 8.402 0 15 6.598 15 15z" fill="#68544F"/>
+                <path d="M497 271h-61c-8.29 0-15-6.71-15-15s6.71-15 15-15h61c8.29 0 15 6.71 15 15s-6.71 15-15 15z" fill="#53433F"/>
+                <path d="M76 271H15c-8.29 0-15-6.71-15-15s6.71-15 15-15h61c8.29 0 15 6.71 15 15s-6.71 15-15 15z" fill="#68544F"/>
+                <path d="M271 15v61c0 8.398-6.598 15-15 15V0c8.402 0 15 6.598 15 15zM271 436v61c0 8.398-6.598 15-15 15v-91c8.402 0 15 6.598 15 15z" fill="#53433F"/>
+                <path d="M358.898 339.398L269.801 131.5c-2.403-5.402-8.102-8.102-13.801-8.102-5.7 0-11.398 2.7-13.8 8.102l-89.098 207.898c-2.704 5.7-1.2 12.301 3.296 16.801 4.504 4.2 11.403 5.399 17.102 2.403L256 317.8l82.5 40.8c5.7 2.997 12.598 1.797 17.102-2.402 4.496-4.5 6-11.101 3.296-16.8z" fill="#70DA40"/>
+                <path d="M355.602 356.2c-4.504 4.198-11.403 5.398-17.102 2.402L256 317.8V123.398c5.7 0 11.398 2.704 13.8 8.102l89.098 207.898c2.704 5.7 1.2 12.301-3.296 16.801z" fill="#49A520"/>
+              </g>
+            </svg>
+
 
           </td>
-          <td>{item.wind.speed}</td>
+          <td>{item.wind.speed.toFixed(0)}</td>
         </tr>
       );
     })
